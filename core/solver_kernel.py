c5f2afb6d1dfd7e37090b979d40f59b065749e66
mamaofthree579-ship-it <mamaofthree579@gmail.com>
2026-03-02 20:22:50 -0500

    Update solver_kernel.py

--- core/solver_kernel.py
+++ core/solver_kernel.py
@@ -2,33 +2,26 @@ import numpy as np
 
 class SolverKernel:
 
-    def __init__(self, grid_shape=(180, 360)):
-
-        self.field = np.random.normal(
-            0, 0.001, grid_shape
-        )
-
-        self.diffusion = 0.15
-        self.damping = 0.25
+    def __init__(self, grid=(180,360)):
+        self.field = np.random.normal(0,0.001,grid)
 
     def laplacian(self, F):
-
         return (
-            np.roll(F, 1, 0) +
-            np.roll(F, -1, 0) +
-            np.roll(F, 1, 1) +
-            np.roll(F, -1, 1) -
-            4 * F
+            np.roll(F,1,0)+
+            np.roll(F,-1,0)+
+            np.roll(F,1,1)+
+            np.roll(F,-1,1)-
+            4*F
         )
 
     def step(self):
 
-        noise = 0.001 * np.random.randn(*self.field.shape)
+        noise = 0.001*np.random.randn(*self.field.shape)
 
-        self.field += 0.01 * (
-            self.diffusion * self.laplacian(self.field)
-            - self.damping * self.field
-            + noise
+        self.field += 0.01*(
+            0.15*self.laplacian(self.field)
+            -0.25*self.field
+            +noise
         )
 
         return self.field
