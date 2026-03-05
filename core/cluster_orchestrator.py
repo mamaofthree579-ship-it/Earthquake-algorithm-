5927b2056fe8805e98e6e4165c2032373860bf1e
mamaofthree579-ship-it <mamaofthree579@gmail.com>
2026-03-05 13:26:00 -0500

    Update cluster_orchestrator.py

--- core/cluster_orchestrator.py
+++ core/cluster_orchestrator.py
@@ -1,54 +1,25 @@
 import uuid
-from concurrent.futures import ThreadPoolExecutor
-from core.artifact_ledger import ArtifactLedger
-from core.reproducibility_engine import ReproducibilityEngine
 
 class ClusterOrchestrator:
 
-    def __init__(self, workers=4):
-        self.executor = ThreadPoolExecutor(max_workers=workers)
+    def __init__(self):
         self.jobs = {}
         self.ledger = ArtifactLedger()
-        self.repro = ReproducibilityEngine(self.ledger)
 
     def submit_job(self, func, params):
 
         job_id = str(uuid.uuid4())
 
-        experiment_hash = self.repro.hash_experiment(func, params)
+        self.jobs[job_id] = "running"
 
-        future = self.executor.submit(self._run_job, job_id, func, params)
+        result = func(params)
 
-        self.jobs[job_id] = {
-            "future": future,
-            "status": "running",
-            "hash": experiment_hash
-        }
+        artifact_id = self.ledger.save(result)
+
+        self.jobs[job_id] = "completed"
 
         return job_id
 
-    def _run_job(self, job_id, func, params):
-
-        result = func(**params)
-
-        self.jobs[job_id]["status"] = "complete"
-
-        artifact_id = self.ledger.store_artifact(
-            job_id,
-            result,
-            self.jobs[job_id]["hash"]
-        )
-
-        return artifact_id
-
     def job_status(self, job_id):
 
-        job = self.jobs.get(job_id)
-
-        if not job:
-            return "unknown"
-
-        if job["future"].done():
-            return "complete"
-
-        return "running"
+        return self.jobs.get(job_id, "unknown")
